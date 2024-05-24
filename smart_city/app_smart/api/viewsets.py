ff01a6f2082d0c4dc1df11d86a4334257334813c
caiofariaas <caiofarias077@gmail.com>
2024-05-24 15:04:27 -0300

    final commit

--- smart_city/app_smart/api/viewsets.py
+++ smart_city/app_smart/api/viewsets.py
@@ -6,6 +6,9 @@ from rest_framework import viewsets
 from app_smart.api.filters import SensorFilter
 from django_filters.rest_framework import DjangoFilterBackend
 
+from rest_framework.views import APIView
+from django.db.models import Q
+
 class CreateUserAPIViewSet(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
@@ -20,3 +23,25 @@ class SensorViewSet(viewsets.ModelViewSet):
     permission_class = [permissions.IsAuthenticated]
     filter_backends = [DjangoFilterBackend]
     filterset_class = SensorFilter
+    
+class SensorFilterView(APIView):
+    permission_classes = ~[permissions.IsAuthenticated]
+    
+    
+    def post (self, request, *args, **kwargs):
+        tipo = request.data.get('tipo', None)
+        localizacao = request.data.get('localizacao', None)
+        responsavel = request.data.get('responsavel', None)
+        status_operacional = request.data.get('status_operacional', None)
+        
+        filters = Q()
+        
+        if tipo:
+            filters &=Q(tipo_icontains = tipo)
+        if localizacao:
+            filters &= Q(localizacao_icontains = localizacao)
+        if responsavel:
+            filters &= Q(responsavel_icontains = responsavel)
+        if status_operacional:
+            filters &= Q(status_operacional_icontains = status_operacional)
+            
