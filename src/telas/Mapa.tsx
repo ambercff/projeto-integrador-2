f2170c9f1f4734a62034f3ad591903cac510ae53
ambercff <ambercf13@gmail.com>
2024-06-06 16:51:43 -0300

    :sparkles: feat: get sensores

--- src/telas/Mapa.tsx
+++ src/telas/Mapa.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Modal, Text, TouchableOpacity, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
@@ -13,19 +13,13 @@ export default function Geo() {
     const [sensorProximo, setSensorProximo] = useState(null);
     const [location, setLocation] = useState(null);
     const [errorMsg, setErrorMsg] = useState(null);
-    const [la, setLa] = useState(null);
-    const [lo, setLo] = useState(null);
     const [distance1, setDistance1] = useState(null); // Distância até o ponto fixo 1
     const [distance2, setDistance2] = useState(null); // Distância até o ponto fixo 2
     const [temp, setTemp] = useState(null);
-    const [x, setX] = useState(null);
     const [modalVisible, setModalVisible] = useState(false);
 
     const [sensores, setSensores] = useState<Sensor[]>([]);
-    const {token} = useAuth();
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-
+    const [fixedPoints, setFixedPoints] = useState([]);
 
     interface Sensor {
         id: number;
@@ -38,7 +32,7 @@ export default function Geo() {
         unidade_medida: string;
         status_operacional: boolean;
         observacao: string;
-      }
+    }
 
     const initialRegion = {
         latitude: -22.9140639,
@@ -51,53 +45,27 @@ export default function Geo() {
         const fetchData = async () => {
             try {
                 const response1 = await axios.get<Sensor>('http://10.0.2.2:8000/api/sensores/1');
-                const response2 = await axios.get<Sensor>('http://10.0.2.2:8000/api/sensores/2');
+                const response2 = await axios.get<Sensor>('http://10.0.2.2:8000/api/sensores/5');
                 setSensores([response1.data, response2.data]);
+                setFixedPoints([
+                    {
+                        id: response1.data.id,
+                        latitude: response1.data.latitude,
+                        longitude: response1.data.longitude,
+                    },
+                    {
+                        id: response2.data.id,
+                        latitude: response2.data.latitude,
+                        longitude: response2.data.longitude,
+                    }
+                ]);
             } catch (error) {
                 console.error(error);
             }
         };
-    
-        fetchData();
         console.log(sensores)
-    }, [sensores]);
-
-    const haversine = (lat1, lon1, lat2, lon2) => {
-        const toRad = (value) => (value * Math.PI) / 180;
-
-        const R = 6371000; // Raio da Terra em metros
-        const dLat = toRad(lat2 - lat1);
-        const dLon = toRad(lon2 - lon1);
-        const a =
-            Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
-            Math.sin(dLon / 2) * Math.sin(dLon / 2);
-        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        const d = R * c; // Distância em metros
-
-        return d;
-    };
- 
-    // const fixedPoints = [
-    //     {
-    //         id: 1,
-    //         latitude: -22.9140639, // Exemplo de coordenada 1
-    //         longitude: -47.068065, // Exemplo de coordenada 1
-    //     },
-    //     {
-    //         id: 2,
-    //         latitude: -22.9141804, // Exemplo de coordenada 2
-    //         longitude: -47.0683294, // Exemplo de coordenada 2
-    //     }
-    // ];
-
-    const fixedPoints = useMemo(() => {
-        return sensores.map(sensor => ({
-            id: sensor.id,
-            latitude: sensor.latitude,
-            longitude: sensor.longitude
-        }));
-    }, [sensores]);
+        fetchData();
+    }, []);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -116,20 +84,6 @@ export default function Geo() {
                     },
                     (newLocation) => {
                         setLocation(newLocation.coords);
-                        setLa(newLocation.coords.latitude);
-                        setLo(newLocation.coords.longitude);
-    
-                        const distanceToFixedPoint1 = haversine(la, lo, fixedPoints[0]['latitude'], fixedPoints[0]['longitude']);
-                        const distanceToFixedPoint2 = haversine(la, lo, fixedPoints[1]['latitude'], fixedPoints[1]['longitude']);
-                        setDistance1(distanceToFixedPoint1);
-                        setDistance2(distanceToFixedPoint2);
-                        if (distanceToFixedPoint1 <= distanceToFixedPoint2) {
-                            const sensor = fixedPoints[1];
-                            setSensorProximo(sensor);
-                        } else {
-                            const sensor = fixedPoints[0];
-                            setSensorProximo(sensor);
-                        }
                     }
                 );
     
@@ -142,8 +96,39 @@ export default function Geo() {
         };
     
         fetchData();
-    }, [sensores, la, lo]);
-    
+    }, []);
+
+    useEffect(() => {
+        if (sensores.length > 0 && location) {
+            const distanceToFixedPoint1 = haversine(location.latitude, location.longitude, fixedPoints[0].latitude, fixedPoints[0].longitude);
+            const distanceToFixedPoint2 = haversine(location.latitude, location.longitude, fixedPoints[1].latitude, fixedPoints[1].longitude);
+            setDistance1(distanceToFixedPoint1);
+            setDistance2(distanceToFixedPoint2);
+            if (distanceToFixedPoint1 <= distanceToFixedPoint2) {
+                const sensor = fixedPoints[1];
+                setSensorProximo(sensor);
+            } else {
+                const sensor = fixedPoints[0];
+                setSensorProximo(sensor);
+            }
+        }
+    }, [sensores, location]);
+
+    const haversine = (lat1, lon1, lat2, lon2) => {
+        const toRad = (value) => (value * Math.PI) / 180;
+
+        const R = 6371000; // Raio da Terra em metros
+        const dLat = toRad(lat2 - lat1);
+        const dLon = toRad(lon2 - lon1);
+        const a =
+            Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
+            Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        const d = R * c; // Distância em metros
+
+        return d;
+    };
 
     let text = 'Waiting...';
     if (errorMsg) {
@@ -152,13 +137,14 @@ export default function Geo() {
         text = `Latitude: ${location.latitude}, Longitude: ${location.longitude}`;
     }
 
+    console.log(fixedPoints)
+
     return (
         <View style={styles.container}>
             <MapView
                 style={styles.map}
                 initialRegion={initialRegion}
             >
-                <Marker coordinate={{ latitude: -22.915, longitude: -47.0678 }} />
                 {fixedPoints.map(point => (
                     <Marker
                         key={point.id}
@@ -175,10 +161,10 @@ export default function Geo() {
             </MapView>
 
             <View style={styles.cxs}>
-                <View style={styles.cx}><Text style={styles.cxTxt}>Latitude: </Text><Text style={styles.cxTxt}>{la}</Text></View>
-                <View style={styles.cx}><Text style={styles.cxTxt}>Longitude: </Text><Text style={styles.cxTxt}>{lo}</Text></View>
+                <View style={styles.cx}><Text style={styles.cxTxt}>Latitude: </Text><Text style={styles.cxTxt}>{location && location.latitude}</Text></View>
+                <View style={styles.cx}><Text style={styles.cxTxt}>Longitude: </Text><Text style={styles.cxTxt}>{location && location.longitude}</Text></View>
                 <View style={styles.cx}><Text style={styles.cxTxt}>Distância até o ponto fixo 1: </Text>{distance1 !== null && <Text style={styles.cxTxt}>{distance1.toFixed(1)} metros</Text>}</View>
-                <View style={styles.cx}><Text style={styles.cxTxt}>Distância até o ponto fixo 2: </Text>{distance1 !== null && <Text style={styles.cxTxt}>{distance1.toFixed(2)} metros</Text>}</View>
+                <View style={styles.cx}><Text style={styles.cxTxt}>Distância até o ponto fixo 2: </Text>{distance2 !== null && <Text style={styles.cxTxt}>{distance2.toFixed(2)} metros</Text>}</View>
                 <View style={styles.cx}><Text style={styles.cxTxt}>Temperatura:</Text><Text style={styles.cxTxt}>{temp}ºC</Text></View>
                 <TouchableOpacity onPress={() => setModalVisible(true)}>
                     <Text style={{ color: 'blue', textDecorationLine: 'underline' }}>Details</Text>
